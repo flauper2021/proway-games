8be1c950df6875c273922ce3f82e0aada9065734
Mauricio Alejandro Juica Arriagada <89223275+flauper2021@users.noreply.github.com>
2023-08-03 16:24:35 -0300

    criando carrinho de compras

--- src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IProduto } from 'src/app/produtos';
+import { CarrinhoService } from 'src/app/carrinho.service';
+import { NotificacaoService } from 'src/app/notificacao.service';
+import { IProduto, IProdutoCarrinho } from 'src/app/produtos';
 import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
@@ -14,7 +16,9 @@ export class DetalhesProdutoComponent implements OnInit {
 
   constructor(
     private produtosService: ProdutosService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private notificacaoService: NotificacaoService,
+    private carrinhoService: CarrinhoService,
   ) { }
 
   ngOnInit(): void {
@@ -22,6 +26,15 @@ export class DetalhesProdutoComponent implements OnInit {
     const produtoId = Number(routeParams.get("id"));
     console.log(produtoId);
     this.produto = this.produtosService.getOne(produtoId);
+  }
+
+  adicionarAoCarrinho(){
+    this.notificacaoService.notificar("O produto foi adicionado ao carrinho!");
+    const produto: IProdutoCarrinho ={
+      ...this.produto!,
+      quantidade: this.quantidade,
+    }
+    this.carrinhoService.adicionarAoCarrinho(produto);
   }
 
 }
