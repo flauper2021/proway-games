a3c575b90e1dc94fd12e5a874e69802af1449484
flauper2021 <mauricioladoalado@yahoo.com>
2023-08-11 13:20:18 -0300

    barra de pesquisa

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CarrinhoService } from '../carrinho.service';
 
 
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
